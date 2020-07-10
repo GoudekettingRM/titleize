86f64d8afb4e6e01009b02e5abe8558600697e00
GoudekettingRM <r.goudeketting@gmail.com>
2020-07-10 08:52:40 +0200

    add two tests

---
+++ test/titleize.spec.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const titleize = require('../titleize');
+
+describe('Titleize tests', () => {
+  describe('Plain tests, without exceptions', () => {
+    it('should return a string without special characters', () => {
+      const str = "I'm a string! Or am I? !@#$%^&*()-_=+[]{};:'\"\\|/.>,<";
+      const title = titleize(str);
+      expect(title).to.equal('Im A String Or Am I');
+    });
+    it('should de-slugify a slug', () => {
+      const str = 'i-am-a-slug-for-your-url';
+      const title = titleize(str);
+      expect(title).to.equal('I Am A Slug For Your Url');
+    });
+  });
+});
