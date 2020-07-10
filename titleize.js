ca200c67b0c0bfcdf1838b89386f9217f1e1abd1
GoudekettingRM <r.goudeketting@gmail.com>
2020-07-10 19:11:26 +0200

    add check for first and last word and capitalise it

--- titleize.js
+++ titleize.js
@@ -1,11 +1,25 @@
 const symbols = /[!¡⁄÷…æ«≤πø¬^˚¨∆¥˙†®´∑œåß∂ƒ∂©˙~µ∫√ç≈§±"#$%&'()*+,./:;<=>¿?@[\\\]^_`{|}~‹™›€£¢∞]/;
 const lowerCasedWords = [
-  'a', 'an', 'the',
-  'and', 'but', 'or', 'nor',
-  'via', 'to', 'on', 'per',
-  'for', 'in', 'of', 'by',
-  'at', 'as', 'yet', 'so'
-]
+  'a',
+  'an',
+  'the',
+  'and',
+  'but',
+  'or',
+  'nor',
+  'via',
+  'to',
+  'on',
+  'per',
+  'for',
+  'in',
+  'of',
+  'by',
+  'at',
+  'as',
+  'yet',
+  'so',
+];
 
 const capitalize = (value) => {
   const splitValue = value.toString().split('');
@@ -35,22 +49,29 @@ const titleize = (value, exceptions = {}) => {
 
   const symbolRegExp = new RegExp('[' + symbolsForRegex + ']', 'gi');
 
-  if (!exceptions.ignoreSymbols || !exceptions.ignoreSymbols.split('').includes('-')) {
+  if (
+    !exceptions.ignoreSymbols ||
+    !exceptions.ignoreSymbols.split('').includes('-')
+  ) {
     value = value.replace(/-/g, ' ');
   }
 
-  return value
+  const valueArray = value
     .toString()
     .replace(symbolRegExp, '')
     .split(' ')
     .filter((word) => {
       if (word) return word;
-    })
-    .map((word) => {
+    });
+
+  return valueArray
+    .map((word, index) => {
       if (word === word.toUpperCase() && exceptions.keepUpperCaseWords) {
-        return word
+        return word;
       } else if (exceptions.keepUpperCaseLetters) {
-        return capitalize(word)
+        return capitalize(word);
+      } else if (index === 0 || index == valueArray.length - 1) {
+        return capitalize(word.toLowerCase());
       } else if (lowerCasedWords.includes(word.toLowerCase())) {
         return word.toLowerCase();
       } else {
