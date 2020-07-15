b12c278abeb06c6a8117a3c70ef7a5c526d43eec
GoudekettingRM <r.goudeketting@gmail.com>
2020-07-15 22:53:40 +0200

    add error when empty string provided

--- test/throwTests.js
+++ test/throwTests.js
@@ -6,40 +6,48 @@ module.exports = {
       input: 10,
       expectedOutput: {
         err: TypeError,
-        msg: 'No string was provided'
-      }
+        msg: 'No string was provided',
+      },
     },
     {
       it: 'should not accept an array as input',
       input: [],
       expectedOutput: {
         err: TypeError,
-        msg: 'No string was provided'
-      }
+        msg: 'No string was provided',
+      },
     },
     {
       it: 'should not accept an array of strings as input',
       input: ['hello', 'world'],
       expectedOutput: {
         err: TypeError,
-        msg: 'No string was provided'
-      }
+        msg: 'No string was provided',
+      },
     },
     {
       it: 'should not accept an abject as input',
       input: {},
       expectedOutput: {
         err: TypeError,
-        msg: 'No string was provided'
-      }
+        msg: 'No string was provided',
+      },
     },
     {
       it: 'should not accept an abject with a string as input',
       input: { hello: 'world' },
       expectedOutput: {
         err: TypeError,
-        msg: 'No string was provided'
-      }
+        msg: 'No string was provided',
+      },
+    },
+    {
+      it: 'should not accept an empty string',
+      input: '',
+      expectedOutput: {
+        err: Error,
+        msg: 'Empty string provided',
+      },
     },
   ],
 };
